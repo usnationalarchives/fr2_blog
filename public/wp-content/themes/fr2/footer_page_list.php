0d22ffa84e6236a69b4cf447fb49775cd811db1c
Andrew Carpenter <acarpen@wested.org>
2011-01-09 12:12:33 -0800

    don't hardcode page ids in theme

--- public/wp-content/themes/fr2/footer_page_list.php
+++ public/wp-content/themes/fr2/footer_page_list.php
@@ -1,13 +1,11 @@
-<div class="grid_3">
-  <h2>Policy</h2>
-  <ul class="bullets">
-    <?php wp_list_pages('title_li=&child_of=2'); ?>
-  </ul>
-</div>
-
-<div class="grid_3">
-  <h2>Learn</h2>
-  <ul class="bullets">
-    <?php wp_list_pages('title_li=&child_of=18'); ?>
-  </ul>
-</div>
+<?php foreach(array('policy','learn') as $slug) { 
+    $page = get_page_by_path($slug);
+    if ($page) { ?>
+    <div class="grid_3">
+      <h2><?= $page->post_title; ?></h2>
+      <ul class="bullets">
+        <?php wp_list_pages('title_li=&depth=1&child_of=' . $page->ID) ?>
+      </ul>
+    </div>
+    <?php } ?>
+<?php } ?>
