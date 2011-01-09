ff62733e9b5dc278a131325fb203bf9bb086c448
Andrew Carpenter <acarpen@wested.org>
2011-01-09 11:45:34 -0800

    move wp-options.yml to config/

--- lib/wordpress.rb
+++ lib/wordpress.rb
@@ -23,11 +23,11 @@ class Wordpress
     Option.dumpable.each do |option|
       options[option.option_name] = option.attributes.except("option_id", "option_name", "blog_id")
     end
-    File.open('wp-options.yml', 'w') {|f| f.write(options.to_yaml) }
+    File.open('config/wp-options.yml', 'w') {|f| f.write(options.to_yaml) }
   end
   
   def load_options
-    YAML::load(File.open('wp-options.yml')).each_pair do |key, attributes|
+    YAML::load(File.open('config/wp-options.yml')).each_pair do |key, attributes|
       opt = Option.find_or_create_by_option_name(key)
       opt.update_attributes(attributes.merge("blog_id" => 0))
     end
