a9c4105aafda7ae369246ab0f66f67335a1cfc34
Dave Augustine <dave@criticaljuncture.org>
2011-01-09 16:03:23 -0800

    A 404 page that is needed but is borking the index for some reason

---
+++ public/wp-content/themes/fr2/504.php
@@ -0,0 +1,15 @@
+<?php 
+/** 
+  * @package WordPress
+  * @subpackage Twenty_Ten
+  * @since Twenty Ten 1.0 
+  */
+
+get_header(); ?>
+
+<h1 class="title"><span>404 Not Found</span></h1>
+
+<h1>The page you were looking for doesn't exist.</h1>
+<h2>You may have mistyped the address or the page may have moved.</h2>
+
+<?php get_footer(); ?>
