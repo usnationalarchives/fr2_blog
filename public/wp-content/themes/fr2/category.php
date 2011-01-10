b2769ab5d9616324374b9cc0b8d7045440e99626
Dave Augustine <dave@criticaljuncture.org>
2011-01-09 20:41:11 -0800

    Fix the category page

--- public/wp-content/themes/fr2/category.php
+++ public/wp-content/themes/fr2/category.php
@@ -20,12 +20,6 @@ get_header(); ?>
 					$category_description = category_description();
 					if ( ! empty( $category_description ) )
 						echo '<div class="archive-meta">' . $category_description . '</div>';
-
-				/* Run the loop for the category page to output the posts.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-category.php and that will be used instead.
-				 */
-				get_template_part( 'loop', 'category' );
 				?>
 
 			</div><!-- #content -->
