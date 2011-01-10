285ab1795646394cf700cf1706e0a4c13441acfa
Dave Augustine <dave@criticaljuncture.org>
2011-01-09 16:02:14 -0800

    Archive page now working - issue with invalid dates displaying all posts due to 404 not working.

--- public/wp-content/themes/fr2/archive.php
+++ public/wp-content/themes/fr2/archive.php
@@ -14,48 +14,35 @@
 
 get_header(); ?>
 <h1 class="title"><span>Blog</span></h1>
-		<div id="container">
-			<div id="content" role="main">
+  <section>
+    <div class="section" id="content_area">
+      <h2 class="page-title">
+        <?php if ( is_day() ) : ?>
+        				<?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
+        <?php elseif ( is_month() ) : ?>
+        				<?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date('F Y') ); ?>
+        <?php elseif ( is_year() ) : ?>
+        				<?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date('Y') ); ?>
+        <?php else : ?>
+        				<?php _e( 'Blog Archives', 'twentyten' ); ?>
+        <?php endif; ?>
+      </h2>
+      <section>
+        <div id="content_area">
 
-<?php
-	/* Queue the first post, that way we know
-	 * what date we're dealing with (if that is the case).
-	 *
-	 * We reset this later so we can run the loop
-	 * properly with a call to rewind_posts().
-	 */
-	if ( have_posts() )
-		the_post();
-?>
+        	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-			<h2 class="page-title">
-<?php if ( is_day() ) : ?>
-				<?php printf( __( 'Daily Archives: <span>%s</span>', 'twentyten' ), get_the_date() ); ?>
-<?php elseif ( is_month() ) : ?>
-				<?php printf( __( 'Monthly Archives: <span>%s</span>', 'twentyten' ), get_the_date('F Y') ); ?>
-<?php elseif ( is_year() ) : ?>
-				<?php printf( __( 'Yearly Archives: <span>%s</span>', 'twentyten' ), get_the_date('Y') ); ?>
-<?php else : ?>
-				<?php _e( 'Blog Archives', 'twentyten' ); ?>
-<?php endif; ?>
-			</h2>
+         	<div class="post" id="post-<?php the_ID(); ?>">
+         			<div><a href="<?php the_permalink();?>"><?php the_title(); ?></a></div><br/>
+         		  <?php the_content();?>
+         	</div>
+          <?php endwhile; else: ?>
+          <?php endif; ?>
 
-<?php
-	/* Since we called the_post() above, we need to
-	 * rewind the loop back to the beginning that way
-	 * we can run the loop properly, in full.
-	 */
-	rewind_posts();
-
-	/* Run the loop for the archives page to output the posts.
-	 * If you want to overload this in a child theme then include a file
-	 * called loop-archives.php and that will be used instead.
-	 */
-	 get_template_part( 'loop', 'archive' );
-?>
-
-			</div><!-- #content -->
-		</div><!-- #container -->
-
+        </div>
+      </section>
+      
+  </div>
+</section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
