3da25ad2537f323a0b8ee04851f7e4d2878b697e
Dave Augustine <dave@criticaljuncture.org>
2011-01-23 15:27:45 -0800

    Search layout

--- public/wp-content/themes/fr2/search.php
+++ public/wp-content/themes/fr2/search.php
@@ -9,29 +9,54 @@
 
 get_header(); ?>
 
-		<div id="container">
-			<div id="content" role="main">
+<h1 class="title"><span><?php bloginfo('name')?></span></h1>
 
-<?php if ( have_posts() ) : ?>
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-				<?php
-				/* Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called loop-search.php and that will be used instead.
-				 */
-				 get_template_part( 'loop', 'search' );
-				?>
+<section>
+  <div id="content_area" class="section blog_post_list">
+
+		<h1 class="page-title"><?php printf( __( 'Search Results for: &nbsp; %s', 'twentyten' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  
+  
+          <article>
+            <div class="article" id="post-<?php the_ID(); ?>">        
+              <div class="info firstchild">
+                <h1 class="firstchild"><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h1>          
+                <div class="summary"><?php the_excerpt(); ?></div>
+                <div class="meta">
+                  <ul>
+                    <li class="author"><?php the_author(); ?></li>
+                    <li class="date"><?php the_date(); ?></li>
+                    <li class="comments"><?php $comments_count = wp_count_comments($post->ID); ?>
+                    <a href="<?php the_permalink();?>#comments" class="comment<?php echo $comments_count->approved == 0 ? ' none' : '';  ?>">
+                    <?php 
+                      echo $comments_count->approved > 0 ? '<span>' . $comments_count->approved . '</span> Comments' : '<span>+</span> Add a comment'; 
+                    ?></a>
+                    </li>
+                </div>
+              </div>
+            </div>    
+          </article>
+
+          <?php endwhile; ?>
+				
 <?php else : ?>
 				<div id="post-0" class="post no-results not-found">
 					<h2 class="entry-title"><?php _e( 'Nothing Found', 'twentyten' ); ?></h2>
-					<div class="entry-content">
-						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
+					<article>
+            <div class="article" id="post-<?php the_ID(); ?>">        
+              <div class="info firstchild">
+    						<p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'twentyten' ); ?></p>
+    						<?php get_search_form(); ?>
+						  </div>
+						</div>
+				  </article>
 				</div><!-- #post-0 -->
 <?php endif; ?>
-			</div><!-- #content -->
-		</div><!-- #container -->
+
+  </div>
+</section> 
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
