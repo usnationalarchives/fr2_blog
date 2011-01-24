c81bbdbc3cb059a537a7255d625f3929cb1cb4e8
Andrew Carpenter <acarpen@wested.org>
2011-01-24 07:46:08 -0800

    show author name not number on homepage esi

--- public/wp-content/themes/fr2/homepage_post_list.php
+++ public/wp-content/themes/fr2/homepage_post_list.php
@@ -11,10 +11,11 @@
     $recent_posts = wp_get_recent_posts(3);
     $i = 0;
     foreach($recent_posts as $post){
+      $post_author = get_userdata($post['post_author']);
     ?>
     <li>  
       <h2><?php echo '<a href="' .get_permalink($post["ID"]) . '" title="Look '.$post["post_title"].'" >' .   $post["post_title"].'</a>' ?></h2>
-      <p class="metadata">Posted by <?php echo $post["post_author"] ?> on <?php echo $post["post_date"] ?></p>
+      <p class="metadata">Posted by <?php echo $post_author->display_name ?> on <?php echo $post["post_date"] ?></p>
       <?php if($i == 0) { ?>
         <p><?php echo my_excerpt( $post["post_content"] ) . '... <a href="'. get_permalink($post["ID"]) . '">Continue reading <span class="meta-nav">&rarr;</span></a>' ?></p>
       <?php 
