e573f5182aec5c57a9207745346258f6621e31c6
Andrew Carpenter <acarpen@wested.org>
2011-01-28 14:51:46 -0800

    fix the footer to actually, you know, work

--- public/blog/wp-content/themes/fr2/footer_page_list.php
+++ public/blog/wp-content/themes/fr2/footer_page_list.php
@@ -6,11 +6,12 @@
 ?>
 <?php foreach(array('policy','learn') as $slug) { 
     $page = get_page_by_path($slug);
+    $children = wp_list_pages('title_li=&depth=1&echo=0&child_of=' . $page->ID);
     if ($page) { ?>
     <div class="grid_3">
       <h2><?= $page->post_title; ?></h2>
       <ul class="bullets">
-        <?php <?php echo preg_replace('/\/blog\//', '/', wp_list_pages('title_li=&depth=1&child_of=' . $page->ID)); ?>
+        <?php echo preg_replace('/\/blog\//', '/', $children); ?>
       </ul>
     </div>
     <?php } ?>
