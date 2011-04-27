0f83e118a91bf98a9ba0ddb934730cba776f4d78
Andrew Carpenter <acarpen@wested.org>
2011-04-26 18:11:36 -0700

    improve search placeholder to make more sense

--- public/blog/wp-content/themes/fr2/searchform.php
+++ public/blog/wp-content/themes/fr2/searchform.php
@@ -1,7 +1,7 @@
 <form action="/blog" method="get" id="blog-search-form">
     <fieldset>
         <label for="search">Search in <?php echo home_url( '/' ); ?></label>
-        <input type="text" name="s" id="blog-search-term" value="<?php the_search_query(); ?>" placeholder="Enter Your Search Terms" />
+        <input type="text" name="s" id="blog-search-term" value="<?php the_search_query(); ?>" placeholder="Search the Blog" />
         <input type="submit" name="search" id="blog-search-submit" />
     </fieldset>
 </form>
