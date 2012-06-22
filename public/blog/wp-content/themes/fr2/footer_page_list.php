c05d2eff73c03a9498ad5a760fd9d4d6e2ba3e9b
Andrew Carpenter <andrew@criticaljuncture.org>
2012-06-22 14:49:28 -0700

    make footer page list span 4 columns

--- public/blog/wp-content/themes/fr2/footer_page_list.php
+++ public/blog/wp-content/themes/fr2/footer_page_list.php
@@ -8,7 +8,7 @@
     $page = get_page_by_path($slug);
     $children = wp_list_pages('title_li=&depth=1&echo=0&child_of=' . $page->ID);
     if ($page) { ?>
-    <div class="grid_3">
+    <div class="grid_4">
       <h2><?= $page->post_title; ?></h2>
       <ul class="bullets">
         <?php echo preg_replace('/\/blog\//', '/', $children); ?>
