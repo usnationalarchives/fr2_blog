666fbd648316a48cc2e721136b2eecc71767fda1
Bob Burbach <bob.burbach@gmail.com>
2012-08-16 11:57:42 -0700

    Add top_nav class to main navigation menu items.

--- public/blog/wp-content/themes/fr2/navigation_page_list.php
+++ public/blog/wp-content/themes/fr2/navigation_page_list.php
@@ -9,7 +9,7 @@
     $children = wp_list_pages('title_li=&depth=1&echo=0&child_of=' . $page->ID);
     if ($page) { ?>
       <li class="dropdown">
-        <a href="#" class="<?= strtolower($page->post_title); ?>"><?= $page->post_title; ?><span class="arrow"></span></a>
+        <a href="#" class="<?= strtolower($page->post_title); ?> top_nav"><?= $page->post_title; ?><span class="arrow"></span></a>
         <ul class="subnav wordpress">
           <?php echo preg_replace('/\/blog\//', '/', $children); ?>
         </ul>
